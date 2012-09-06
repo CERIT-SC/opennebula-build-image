140794e1ffc1b386f09aa2f3dee935c7c6760fb5
Vlastimil Holer <vlastimil.holer@gmail.com>
2012-09-06 18:32:14 +0200

    New README.mk

---
+++ README.mk
@@ -0,0 +1,15 @@
+# Image build scripts for CERIT-SC's OpenNebula KVM cloud
+
+### Requirements
+
+Installed and configured:
+
+* one tools (oneimage)
+* OCCI tools (occi-storage)
+
+# Quick Start
+
+Build and upload new/updated Debian 6:
+
+    cd debian6
+    make
