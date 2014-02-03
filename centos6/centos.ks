7fd8e178ddac2d665d62573159bfc58433e3c125
Vlastimil Holer <holer@ics.muni.cz>
2014-02-03 15:08:36 +0100

    Update Debian 7 and CentOS 6 builds.

--- centos6/centos.ks
+++ centos6/centos.ks
@@ -10,12 +10,12 @@ network --device=eth0 --bootproto dhcp --onboot=yes
 network --device=eth1 --bootproto dhcp --onboot=yes
 skipx
 cmdline
-reboot
+poweroff
 
 # security
-rootpw      --iscrypted *
+rootpw      --lock --iscrypted *
 authconfig	--enableshadow --passalgo=sha512
-firewall    --enabled --ssh
+firewall    --disabled
 selinux     --permissive
 
 # Partitioning, bootloader
@@ -25,12 +25,12 @@ clearpart --all --initlabel
 part /boot --fstype=ext4 --size=256 --label=boot
 part pv.01 --size=1024 --grow
 volgroup VolGroup pv.01
-logvol / --fstype=ext4	--name=root --vgname=VolGroup --size=512 --grow
+logvol / --fstype=ext4	--name=root --vgname=VolGroup --size=512 --grow --fsoptions="defaults,usrjquota=aquota.user,grpjquota=aquota.group,jqfmt=vfsv0"
 bootloader --location=mbr --append="elevator=deadline console=tty0 console=ttyS0,115200"
 
 # Repo: CentOS url --url http://mirror.centos.org/centos/6/os/x86_64/
-repo --name=updates	--baseurl=http://mirror.centos.org/centos/6/updates/x86_64/
-repo --name=extras	--baseurl=http://mirror.centos.org/centos/6/extras/x86_64/
+repo --name=updates --baseurl=http://mirror.centos.org/centos/6/updates/x86_64/
+repo --name=extras  --baseurl=http://mirror.centos.org/centos/6/extras/x86_64/
 
 %packages
 @core
@@ -40,59 +40,52 @@ mc
 dstat
 xorg-x11-xauth
 xterm
+%end
 
-%post --nochroot
-tar --no-same-owner -xvf /cloud.tar -C /mnt/sysimage/etc/
-cp /cloud-init-el6.rpm /mnt/sysimage/tmp
+%post --nochroot --erroronfail
+set -e
+install -Dp --mode=644 /cloud.cfg /mnt/sysimage/etc/cloud/cloud.cfg
+install -Dp --mode=644 /RPM-GPG-KEY-EPEL-6 /mnt/sysimage/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6
+install -Dp --mode=644 /RPM-GPG-KEY-CERIT-SC /mnt/sysimage/etc/pki/rpm-gpg/RPM-GPG-KEY-CERIT-SC
+quotacheck -vcguma
+chroot /mnt/sysimage restorecon -Fi aquota.user aquota.group
+quotaon -a
+%end
 
-%post
+%post --erroronfail
+set -e
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CERIT-SC
+
+cat <<EOF >/etc/yum.repos.d/epel.repo
+[epel]
+name=Extra Packages for Enterprise Linux 6 - \$basearch
+mirrorlist=https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=\$basearch
+failovermethod=priority
+enabled=1
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6
+EOF
+
+cat <<EOF >/etc/yum.repos.d/cloud-init.repo
+[cloud-init]
+name=CERIT-SC cloud-init RPM repository
+baseurl=http://yum.cerit-sc.cz/cloud-init/el/\$releasever
+enabled=1
+gpgcheck=1
+EOF
+
+yum -y install cloud-init
+yum clean all
+
 # https://bugzilla.redhat.com/show_bug.cgi?id=510523
-sed -i 's/rhgb//'	/etc/grub.conf
+sed -i 's/rhgb//' /boot/grub/grub.conf
 sed -i '/^HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-eth*
 
 # https://bugzilla.redhat.com/show_bug.cgi?id=756130
+set +e
 unlink /etc/udev/rules.d/70-persistent-net.rules
 ln -s /dev/null /etc/udev/rules.d/70-persistent-net.rules
 unlink /etc/udev/rules.d/70-persistent-cd.rules
 ln -s /dev/null /etc/udev/rules.d/70-persistent-cd.rules
-
-set -e
-
-# setup EPEL repository
-EPEL_RELEASE_URL='http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm'
-EPEL_RELEASE=$(mktemp)
-EPEL_GPG_KEY=$(mktemp)
-cat <<EOF >${EPEL_GPG_KEY}
------BEGIN PGP PUBLIC KEY BLOCK-----
-Version: SKS 1.1.3
-
-mQINBEvSKUIBEADLGnUj24ZVKW7liFN/JA5CgtzlNnKs7sBg7fVbNWryiE3URbn1JXvrdwHt
-kKyY96/ifZ1Ld3lE2gOF61bGZ2CWwJNee76Sp9Z+isP8RQXbG5jwj/4BM9HK7phktqFVJ8Vb
-Y2jfTjcfxRvGM8YBwXF8hx0CDZURAjvf1xRSQJ7iAo58qcHnXtxOAvQmAbR9z6Q/h/D+Y/Ph
-oIJp1OV4VNHCbCs9M7HUVBpgC53PDcTUQuwcgeY6pQgo9eT1eLNSZVrJ5Bctivl1UcD6P6CI
-GkkeT2gNhqindRPngUXGXW7Qzoefe+fVQqJSm7Tq2q9oqVZ46J964waCRItRySpuW5dxZO34
-WM6wsw2BP2MlACbH4l3luqtpXo3Bvfnk+HAFH3HcMuwdaulxv7zYKXCfNoSfgrpEfo2Ex4Im
-/I3WdtwME/Gbnwdq3VJzgAxLVFhczDHwNkjmIdPAlNJ9/ixRjip4dgZtW8VcBCrNoL+LhDrI
-fjvnLdRuvBHy9P3sCF7FZycaHlMWP6RiLtHnEMGcbZ8QpQHi2dReU1wyr9QgguGU+jqSXYar
-1yEcsdRGasppNIZ8+Qawbm/a4doT10TEtPArhSoHlwbvqTDYjtfV92lC/2iwgO6gYgG9XrO4
-V8dV39Ffm7oLFfvTbg5mv4Q/E6AWo/gkjmtxkculbyAvjFtYAQARAQABtCFFUEVMICg2KSA8
-ZXBlbEBmZWRvcmFwcm9qZWN0Lm9yZz6JAjYEEwECACAFAkvSKUICGw8GCwkIBwMCBBUCCAME
-FgIDAQIeAQIXgAAKCRA7Sd8qBgi4lR/GD/wLGPv9qO39eyb9NlrwfKdUEo1tHxKdrhNz+XYr
-O4yVDTBZRPSuvL2yaoeSIhQOKhNPfEgT9mdsbsgcfmoHxmGVcn+lbheWsSvcgrXuz0gLt8TG
-GKGGROAoLXpuUsb1HNtKEOwPQ4z1uQ2nOz5hLRyDOV0I2LwYV8BjGIjBKUMFEUxFTsL7XOZk
-rAg/WbTH2PW3hrfSWtcRA7EYonI3B80d39ffws7SmyKbS5PmZjqOPuTvV2F0tMhKIhncBwoo
-jWZPExftHpKhzKVh8fdDO/3P1y1Fk3Cin8UbCO9MWMFNR27fVzCANlEPljsHA+3Ez4F7uboF
-p0OOEov4Yyi4BEbgqZnthTG4ub9nyiupIZ3ckPHr3nVcDUGcL6lQD/nkmNVIeLYPx1uHPOSl
-WfuojAYgzRH6LL7Idg4FHHBA0to7FW8dQXFIOyNiJFAOT2j8P5+tVdq8wB0PDSH8yRpn4HdJ
-9RYquau4OkjluxOWf0uRaS//SUcCZh+1/KBEOmcvBHYRZA5Jl/nakCgxGb2paQOzqqpOcHKv
-lyLuzO5uybMXaipLExTGJXBlXrbbASfXa/yGYSAGiVrGz9CE6676dMlm8F+s3XXE13QZrXmj
-loc6jwOljnfAkjTGXjiB7OULESed96MRXtfLk0W5Ab9pd7tKDR6QHI7rgHXfCopRnZ2VVQ==
-=V/6I
------END PGP PUBLIC KEY BLOCK-----
-EOF
-
-rpm --import ${EPEL_GPG_KEY} &&
-	wget -O "${EPEL_RELEASE}" "${EPEL_RELEASE_URL}" &&
-	rpm --checksig "${EPEL_RELEASE}" &&
-	rpm -Uvh "${EPEL_RELEASE}" &&
-	yum -y install /tmp/cloud-init-el6.rpm
+%end
--- common.mk
+++ common.mk
@@ -23,7 +23,7 @@ VI?=virt-install \
 	--noreboot
 VI_NAME?=build-$(ON_PREFIX)
 VI_CPU?=2
-VI_RAM?=1024
+VI_RAM?=1536
 VI_TIMEOUT?=45
 
 # qemu-image
@@ -32,17 +32,22 @@ QI_QCOW_OPTS?=-c -o cluster_size=2M
 QI_VMDK_OPTS?=
 
 # ON ... OpenNebula image
-ON_DATA_STORES?=cerit-sc-zegox cerit-sc-cloud cerit-sc-ha-jihlava cerit-sc-zigur_zapat
+ON_DS?=cerit-sc-zegox cerit-sc-ha-brno cerit-sc-ha-jihlava cerit-sc-zigur_zapat
+ON_PUBLIC_DS?=cerit-sc-cloud
+
 ON_PREFIX?=$(OS)-$(OS_VERSION)
 ON_NAME?=$(ON_PREFIX)-$(ON_VERSION)
-ON_DESCRIPTION?=OpenNebula Image
+ON_DESCRIPTION?=Generic OS image by CERIT Scientific Cloud
 ON_VERSION:=$(shell oneimage list -x 2>/dev/null | egrep -xi '\s*<NAME>$(ON_PREFIX)-[0-9]+[^<]+</NAME>\s*' | sed -e 's/\s*<NAME>$(ON_PREFIX)-\([0-9]*\)[^<]*<\/NAME>\s*/\1/' | awk 'BEGIN{max=0} {if ($$0>max) max=$$0} END{printf "%04i",max+1}')
 ON_SOURCE?=$(shell oneimage show $(ON_NAME) | awk -F'[ ]*:[ ]*' '$$1=="SOURCE" { print $$2 }')
 ON_DEV_PREFIX?=vd
-ON_DRIVER?=qcow2
 ON_PERSISTENT?=no
+ON_OWNER?=cerit-sc-admin
 ON_GROUP?=cerit-sc
-ON_MODE?=644
+
+# ON upload variables
+SCP_HOST?=cerit-sc-admin-api@carach4.ics.muni.cz
+SCP_PATH?=/home/cerit-sc-admin-api
 
 # M4 templates
 M4_OCCI_STORAGE?=../occi-storage.m4
@@ -65,42 +70,100 @@ $(IMAGE).qcow2: $(IMAGE).raw
 $(IMAGE).vmdk: $(IMAGE).raw
 	qemu-img convert $(QI_VMDK_OPTS) -O vmdk $? $@
 
-# upload image to default datastore through OCCI
+## upload image to default datastore through OCCI
+#$(ON_NAME): $(IMAGE).$(FORMAT)
+#	if ! oneimage show $(ON_NAME); then \
+#		set -e; \
+#		DEF=`mktemp`; \
+#		m4 -D__NAME__="$(ON_NAME)" \
+#			-D__DESCRIPTION__="$(ON_DESCRIPTION)" \
+#			-D__URL__="file://$?" \
+#			$(M4_OCCI_STORAGE) | tee $$DEF; \
+#		occi-storage create $$DEF; \
+#		unlink $$DEF; \
+#	fi
+
+# upload image to ON server
 $(ON_NAME): $(IMAGE).$(FORMAT)
-	if ! oneimage show $(ON_NAME); then \
-		set -e; \
-		DEF=`mktemp`; \
-		m4 -D__NAME__="$(ON_NAME)" \
-			-D__DESCRIPTION__="$(ON_DESCRIPTION)" \
-			-D__URL__="file://$?" \
-			$(M4_OCCI_STORAGE) | tee $$DEF; \
-		occi-storage create $$DEF; \
-		unlink $$DEF; \
-	fi
+	scp -o 'Compression no' -o 'Cipher blowfish' \
+		$? $(SCP_HOST):$(SCP_PATH)/$(ON_NAME).$(FORMAT)
 
-# copy image from default to specified datastore
+## copy image from default to specified datastore
+#$(ON_NAME)@%: $(ON_NAME)
+#	if ! oneimage show $@; then \
+#		set -e; \
+#		DEF=`mktemp`; \
+#		m4 -D__NAME__="$@" \
+#			-D__DESCRIPTION__="$(ON_DESCRIPTION)" \
+#			-D__PATH__="$(ON_SOURCE)" \
+#			-D__DRIVER__="$(ON_DRIVER)" \
+#			-D__DEV_PREFIX__="$(ON_DEV_PREFIX)" \
+#			-D__PERSISTENT__="$(ON_PERSISTENT)" \
+#			$(M4_ONEIMAGE) | tee $$DEF; \
+#		oneimage create $$DEF --datastore $*; \
+#		oneimage chgrp "$@" $(ON_GROUP) ; \
+#		oneimage chmod "$@" $(ON_MODE) ; \
+#		unlink $$DEF; \
+#	fi
+
+#define oneimage
+#	oneimage create -v --name "$1" \
+#		--source="$(SCP_PATH)/$(ON_NAME).$(FORMAT)" \
+#		--prefix=vd --type=OS  --driver=$(FORMAT) \
+#		--datastore="$2" --disk_type="BLOCK" \
+#		--size=$$(($(SIZE) * 1024)) \
+#		--description="$(ON_DESCRIPTION)"
+#	oneimage chown "$1" $(ON_OWNER)
+#	oneimage chgrp "$1" $(ON_GROUP)
+#	oneimage chmod "$1" $3
+#endef
+
+define oneimage 
+	set -e; \
+	DEF=`mktemp`; \
+	m4 -D__NAME__="$1" \
+		-D__DESCRIPTION__="$(ON_DESCRIPTION)" \
+		-D__PATH__="$(SCP_PATH)/$(ON_NAME).$(FORMAT)" \
+		-D__DRIVER__="$(FORMAT)" \
+		-D__DEV_PREFIX__="$(ON_DEV_PREFIX)" \
+		-D__PERSISTENT__="$(ON_PERSISTENT)" \
+		$(M4_ONEIMAGE) | tee $$DEF; \
+	oneimage create $$DEF --datastore $2; \
+	oneimage chown "$1" $(ON_OWNER) ; \
+	oneimage chgrp "$1" $(ON_GROUP) ; \
+	oneimage chmod "$1" $3 ; \
+	unlink $$DEF
+endef
+
+#$(ON_NAME)@%: $(ON_NAME)
+#	oneimage create -v --name "$@" \
+#		--source="$(ON_PATH)/$(ON_NAME).$(FORMAT)" \
+#		--prefix=vd --type=OS  --driver=$(FORMAT) \
+#		--datastore="$*" --disk_type="BLOCK" \
+#		--size=$$(($(SIZE) * 1024)) \
+#		--description="Generic OS image by CERIT Scientific Cloud"
+#	oneimage chown "$@" $(ON_OWNER)
+#	oneimage chgrp "$@" $(ON_GROUP)
+#	oneimage chmod "$@" $(ON_MODE)
+
 $(ON_NAME)@%: $(ON_NAME)
-	if ! oneimage show $@; then \
-		set -e; \
-		DEF=`mktemp`; \
-		m4 -D__NAME__="$@" \
-			-D__DESCRIPTION__="$(ON_DESCRIPTION)" \
-			-D__PATH__="$(ON_SOURCE)" \
-			-D__DRIVER__="$(ON_DRIVER)" \
-			-D__DEV_PREFIX__="$(ON_DEV_PREFIX)" \
-			-D__PERSISTENT__="$(ON_PERSISTENT)" \
-			$(M4_ONEIMAGE) | tee $$DEF; \
-		oneimage create $$DEF --datastore $*; \
-		oneimage chgrp "$@" $(ON_GROUP) ; \
-		oneimage chmod "$@" $(ON_MODE) ; \
-		unlink $$DEF; \
-	fi
+	$(call oneimage,$@,$*,640)
 
-upload: $(patsubst %, $(ON_NAME)@%, $(ON_DATA_STORES))
+$(ON_NAME)@%.public: $(ON_NAME)
+	$(call oneimage,$(ON_NAME)@$*,$*,644)
 
-clean: 
+upload: \
+		$(patsubst %, $(ON_NAME)@%.public, $(ON_PUBLIC_DS)) \
+		$(patsubst %, $(ON_NAME)@%, $(ON_DS))
+
+empty:
+	echo -n >empty
+
+clean: empty
 	-virsh -q -c $(LIBVIRT_URI) destroy  $(VI_NAME) 2>/dev/null
 	-virsh -q -c $(LIBVIRT_URI) undefine $(VI_NAME) 2>/dev/null 
-	-oneimage delete $(ON_NAME)
+#	-oneimage delete $(ON_NAME)
+	-scp -o 'Compression no' -o 'Cipher blowfish' \
+		empty $(SCP_HOST):$(SCP_PATH)/$(ON_NAME).$(FORMAT)
 	rm -f $(IMAGE).raw $(IMAGE).raw.tmp $(IMAGE).$(FORMAT) \
-		$(IMAGE).qcow2 $(IMAGE).vmdk cloud.tar
+		$(IMAGE).qcow2 $(IMAGE).vmdk cloud.tar empty
